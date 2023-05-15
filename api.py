a440ff2372fbb4439ed0415aa6ac2bdbec089119
Shubhanshi Singh <singhshubhanshi1701@gmail.com>
2023-05-16 02:19:42 +0530

    second commit

--- api.py
+++ api.py
@@ -3,12 +3,12 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-from keras.models import load_model
+from tensorflow.python.keras.models import load_model
 
 #%%
 
 # Load the trained model
-model = load_model('path_to_your_trained_model.h5')
+model = load_model('C:/Users/shubh/DogEmotionClassification/model_weights.h5')
 
 # Define the Flask app
 app = Flask(__name__)
@@ -22,16 +22,16 @@ def classify_emotion():
 		return jsonify({'error': 'No image file provided'})
 
 	# Read the image file and preprocess it
-	image_file = request.files['image']
+	image_file = request.files['./small-fuffy-dog-breeds-1623362663.jpg']
 	image = Image.open(image_file)
-	image = image.resize((224, 224))  # Resize to match the input size expected by the model
+	image = image.resize((96, 96))  # Resize to match the input size expected by the model
 	image = np.array(image) / 255.0  # Normalize pixel values between 0 and 1
 	image = np.expand_dims(image, axis=0)  # Add a batch dimension
 
 	# Make predictions using the model
 	predictions = model.predict(image)
 	predicted_class = np.argmax(predictions[0])
-	emotion_labels = ['Happy', 'Sad', 'Angry']
+	emotion_labels = ['angry', 'happy', 'sad']
 	predicted_emotion = emotion_labels[predicted_class]
 
 	# Return the classification result as a JSON response
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import numpy as np
 # import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from flask import Flask, request, jsonify
-from PIL import Image
-from keras.models import load_model
 
 from keras.applications.vgg19 import VGG19
 from keras.models import Sequential
@@ -94,12 +91,12 @@ data_with_aug = ImageDataGenerator(horizontal_flip=True,
 train = data_with_aug.flow_from_directory(dataset_path,
                                           class_mode="binary",
                                           target_size=(96, 96),
-                                          batch_size=32,
+                                          batch_size=64,
                                           subset="training")
 val = data_with_aug.flow_from_directory(dataset_path,
                                         class_mode="binary",
                                         target_size=(96, 96),
-                                        batch_size=32,
+                                        batch_size=64,
                                         subset="validation"
                                         )
 # %%
@@ -166,16 +163,16 @@ plt.grid(True)
 plt.legend(['train', 'val'], loc=4)
 plt.style.use(['classic'])
 
-model.save_weights('model_weights.h5')
+model.save_weights('./model_weights.h5')
 
 # %%
-predictions = model.predict_generator(val)
+predictions = model.predict(val)
 
 # %%
 
 val_path = "./images/"
 
-plt.figure(figsize=(15, 15))
+plt.figure(figsize=(10, 10))
 
 start_index = 250
 
@@ -201,6 +198,13 @@ for i in range(16):
 	else:
 		col = "g"
 
+	if gt == "angry":
+		gt = 'angry'
+	elif gt == 'happy':
+		gt = 'happy'
+	else:
+		gt = 'sad'
+
 	plt.xlabel('i={}, pred={}, gt={}'.format(start_index + i, pred, gt), color=col)
 	plt.imshow(load_path(val_path + val.filenames[start_index + i]))
 	plt.tight_layout()
@@ -208,4 +212,38 @@ for i in range(16):
 plt.show()
 
 # %%
+from flask import Flask, request, jsonify
+from PIL import Image
+
+# Define the Flask app
+app = Flask(__name__)
+
+
+# API endpoint for classifying dog emotions
+@app.route('/classify', methods=['POST'])
+def classify_emotion():
+	# Check if an image file was included in the request
+	if 'image' not in request.files:
+		return jsonify({'error': 'No image file provided'})
+
+	# Read the image file and preprocess it
+	image_file = request.files['image']
+	image = Image.open(image_file)
+	image = image.resize((96, 96))  # Resize to match the input size expected by the model
+	image = np.array(image) / 255.0  # Normalize pixel values between 0 and 1
+	image = np.expand_dims(image, axis=0)  # Add a batch dimension
+
+	# Make predictions using the model
+	predictions = model.predict(image)
+	predicted_class = np.argmax(predictions[0])
+	emotion_labels = ['angry', 'happy', 'sad']
+	predicted_emotion = emotion_labels[predicted_class]
+
+	# Return the classification result as a JSON response
+	return jsonify({'emotion': predicted_emotion})
+
+
+# Run the Flask app
+if __name__ == '__main__':
+	app.run()
 
